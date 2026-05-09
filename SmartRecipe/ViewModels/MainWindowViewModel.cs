d4a7a8b950427e847c48b01868872638de818868
Ростислав <c.paziak.rostyslav@student.uzhnu.edu.ua>
2026-05-09 19:05:43 +0300

    Initial commit

---
+++ SmartRecipe/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace SmartRecipe.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ SmartRecipe/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SmartRecipe.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
